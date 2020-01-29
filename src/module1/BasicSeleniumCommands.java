02db89101933faed7194dff73e59ec1af69620f8
Lavanya Ung <analyst.lavanya@gmail.com>
2020-01-28 19:15:48 -0500

    my first git commit

---
+++ src/module1/BasicSeleniumCommands.java
@@ -0,0 +1,18 @@
+package module1;
+
+public class BasicSeleniumCommands {
+
+	public static void main(String[] args) {
+		
+    Object webDriver;
+		//single line comments
+	/*
+	 * multi line comments
+	 */
+	//initialize or instantiate or open a browser
+		System.setProperty(webDriver.chrome.driver,"C:\\Users\\ungar\\Edureka Selenium\\chromedriver.exe" );
+		webdriver driver=new ChromeDriver();
+		
+	}
+
+}
